3bbbb409a1b6b7a27791a7346d0bbbb3559c8eea
Eduard Schildner <eduard.schildner@gmail.com>
2023-05-18 12:37:46 +0200

    Update setup script.

--- setup_project.sh
+++ setup_project.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
 source ./variables.sh
 
+# Create Function App
 az group create -n "${RG_NAME}" -l "${LOCATION}"
 az storage account create -n "${STORAGE_ACCOUNT_NAME}" --location "${LOCATION}" -g "${RG_NAME}" --sku Standard_LRS
 az functionapp create \
@@ -13,3 +19,69 @@ az functionapp create \
     --functions-version 4 \
     --os-type linux \
     -s "${STORAGE_ACCOUNT_NAME}"
+
+# Create a Cosmos account for MongoDB
+az cosmosdb create \
+    -n "${COSMOS_DB_ACCOUNT_NAME}" \
+    -g "${RG_NAME}" \
+    --kind MongoDB \
+    --server-version "${DB_SERVER_VERSION}" \
+    --default-consistency-level Eventual \
+    --enable-automatic-failover false
+
+# Create a JSON file in the current directory defining the index policy for the collection.
+# The indexing policy file defines which field to create an index on for faster search/read.
+printf ' 
+[ 
+    {
+        "key": {"keys": ["_id"]}
+    }
+]' > "idxpolicy-${SUFFIX}.json"
+
+# Create a MongoDB API Collections
+az cosmosdb mongodb collection create \
+    -a "${COSMOS_DB_ACCOUNT_NAME}" \
+    -g "${RG_NAME}" \
+    -d "${DB_NAME}"  \
+    -n "${COLLECTION_1}" \
+    --shard '_id' \
+    --throughput 400 \
+    --idx "@idxpolicy-${SUFFIX}.json"
+
+az cosmosdb mongodb collection create \
+    -a "${COSMOS_DB_ACCOUNT_NAME}" \
+    -g "${RG_NAME}" \
+    -d "${DB_NAME}"  \
+    -n "${COLLECTION_2}" \
+    --shard '_id' \
+    --throughput 400 \
+    --idx "@idxpolicy-${SUFFIX}.json"
+
+echo "Connection strings can be obtained as follows:"
+echo "az cosmosdb keys list -n ${COSMOS_DB_ACCOUNT_NAME} -g ${RG_NAME} --type connection-strings"
+
+# Fetch and store the connection string
+DB_CONNECTION_STRING=$(az cosmosdb keys list \
+--type connection-strings \
+-n "${COSMOS_DB_ACCOUNT_NAME}" \
+-g "${RG_NAME}" \
+--query 'connectionStrings[0].connectionString' \
+--output tsv)
+echo The connection string will be needed later to connect the app with the DB, so make sure to save it somewhere.
+echo -e "${YELLOW}${DB_CONNECTION_STRING}${NC}"
+
+# Import data into the collections
+mongoimport --uri "${DB_CONNECTION_STRING}" \
+    --db "${DB_NAME}" \
+    --collection "${COLLECTION_1}" \
+    --file='./sample_data/sampleAds.json' \
+    --jsonArray
+
+mongoimport --uri "${DB_CONNECTION_STRING}" \
+    --db "${DB_NAME}" \
+    --collection "${COLLECTION_2}" \
+    --file='./sample_data/samplePosts.json' \
+    --jsonArray
+
+echo "To delete all resources, run the following command:"
+echo -e "${RED}az group delete --name ${RG_NAME} --no-wait --yes${NC}"
--- variables.sh
+++ variables.sh
@@ -2,8 +2,15 @@
 
 # This file contains the variables used in the project.
 
-SUFFIX="-es81" # for resources that need a unique name
-RG_NAME="neighborly-rg"
+SUFFIX="es81" # for resources that need a unique name
+RG_NAME="nd081-project2-rg"
 LOCATION="eastus"
-STORAGE_ACCOUNT_NAME="neighborlysa${SUFFIX}"
-FUNCTION_APP_NAME="neighborlyapp${SUFFIX}}"
+STORAGE_ACCOUNT_NAME="sa${SUFFIX}"
+FUNCTION_APP_NAME="neighborlyapp-${SUFFIX}"
+
+# cosmosDBAccountName needs to be lower case
+COSMOS_DB_ACCOUNT_NAME="cosmos-db-account-${SUFFIX}"
+DB_SERVER_VERSION="3.6"
+DB_NAME="neighborlydb"
+COLLECTION_1="advertisements"
+COLLECTION_2="posts"
