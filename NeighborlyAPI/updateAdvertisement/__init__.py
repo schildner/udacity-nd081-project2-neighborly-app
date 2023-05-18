083c736151dab40e369902592b3c12c4d1d2a665
Eduard Schildner <eduard.schildner@gmail.com>
2023-05-18 12:49:16 +0200

    Update Db name to connect functions to.

--- NeighborlyAPI/updateAdvertisement/__init__.py
+++ NeighborlyAPI/updateAdvertisement/__init__.py
@@ -11,9 +11,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         try:
             url = "mongodb://cosmos-db-account-es81:Mqxk0U1ppq1aMCkCBB7yF5rG6F8gLs8ocyhyPihXkchj9i3nexJfvQs4rRzcp2Kq4qVAh7TonNOfACDbFf5FAw==@cosmos-db-account-es81.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-account-es81@"
             client = pymongo.MongoClient(url)
-            database = client['azure']
+            database = client['neighborlydb']
             collection = database['advertisements']
-            
+
             filter_query = {'_id': ObjectId(id)}
             update_query = {"$set": eval(request)}
             rec_id1 = collection.update_one(filter_query, update_query)
