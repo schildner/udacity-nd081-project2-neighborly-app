47bfc0ab6acf59846c510cedff58b666c1fd0629
Eduard Schildner <eduard.schildner@gmail.com>
2023-05-20 13:49:20 +0200

    Change db details and connection strings.

--- NeighborlyAPI/getAdvertisement/__init__.py
+++ NeighborlyAPI/getAdvertisement/__init__.py
@@ -14,9 +14,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "mongodb://cosmos-db-account-es81:Mqxk0U1ppq1aMCkCBB7yF5rG6F8gLs8ocyhyPihXkchj9i3nexJfvQs4rRzcp2Kq4qVAh7TonNOfACDbFf5FAw==@cosmos-db-account-es81.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-account-es81@"
+            url = "mongodb://cosmos-db-account1-es81:VBq8TJoMuEZdAyOAskN5WAnz1QaKrQO7peGetkVFrwTMotAHUshw5HGZ7BPfYWEZVCnjIXsVwPMmACDbeURyFA==@cosmos-db-account1-es81.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-account1-es81@"
             client = pymongo.MongoClient(url)
-            database = client['neighborlydb']
+            database = client['dbes81']
             collection = database['advertisements']
 
             query = {'_id': ObjectId(id)}
