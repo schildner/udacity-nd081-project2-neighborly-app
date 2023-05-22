86f45f55cd646bc6dea0b626fc2023bd06eeb440
Eduard Schildner <eduard.schildner@gmail.com>
2023-05-22 10:12:25 +0200

    Update conn string after redeployment of cosmos db account

--- NeighborlyAPI/getAdvertisement/__init__.py
+++ NeighborlyAPI/getAdvertisement/__init__.py
@@ -14,7 +14,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "mongodb://cosmos-db-account1-es81:VBq8TJoMuEZdAyOAskN5WAnz1QaKrQO7peGetkVFrwTMotAHUshw5HGZ7BPfYWEZVCnjIXsVwPMmACDbeURyFA==@cosmos-db-account1-es81.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-account1-es81@"
+            url = "mongodb://cosmos-db-account-es81:9n6ytBSPTUdj5W6Ne7ywfnUsV9AbLKSJegjKWl2PhjV0PAJ58pgQKKCkKRPFB2ZrXfum4BFdHTuVACDbeZVYfQ==@cosmos-db-account-es81.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-account-es81@"
             client = pymongo.MongoClient(url)
             database = client['dbes81']
             collection = database['advertisements']
